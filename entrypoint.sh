99b5d26e36be1a1eaca10dcaaf50face58c0d4ea
Michal Charemza <michal@charemza.name>
2018-09-04 16:44:10 +0100

    (feat) Be explicit to listen on all interfaces, since I suspect the previous didn't work when in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,6 @@ sed -i -e "s%__JPYNB_S3_BUCKET_NAME__%${JPYNB_S3_BUCKET_NAME}%g" /usr/local/etc/
 
 rsync -auzv /usr/local/etc/jupyter/ /etc/jupyter/
 
-jupyterhub --config=/etc/jupyter/jupyterhub_config.py --JupyterHub.hub_ip="$(curl -Lfs http://169.254.169.254/latest/meta-data/local-ipv4)"
+# hub_ip is the interface that the hub listens on, 0.0.0.0 == all
+# hub_connect_ip is the IP that _other_ services will connect to the hub on, i.e. the current private IP address
+jupyterhub --config=/etc/jupyter/jupyterhub_config.py --JupyterHub.hub_ip="0.0.0.0" --JupyterHub.hub_connect_ip="$(curl -Lfs http://169.254.169.254/latest/meta-data/local-ipv4)"

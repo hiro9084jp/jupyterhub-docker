c7572ed42b96d4d2abd1dd43b1f0d9cd979e9b86
Michal Charemza <michal@charemza.name>
2018-09-11 16:08:04 +0100

    (feat) Communicate to the notebook server using HTTPS
    Although I'm wary of making things _too_ configurable without cause, there is a
    tacit requirement to make the EcsSpawner a library that others can use. Mostly,
    I suspect there will _not_ be HTTPS communication to the Notebook, so from the
    point of the EcsSpawner, it's configurable.

--- config/jupyter_notebook_config.py
+++ config/jupyter_notebook_config.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from s3contents import S3ContentsManager
 c = get_config()
 
@@ -8,3 +9,14 @@ c.NotebookApp.contents_manager_class = S3ContentsManager
 
 c.S3ContentsManager.prefix = os.environ['JUPYTERHUB_USER']
 c.S3ContentsManager.sse = "AES256"
+
+keyfile = '/etc/jupyter/ssl.key'
+certfile = '/etc/jupyter/ssl.crt'
+subprocess.check_call([
+    'openssl', 'req', '-new', '-newkey', 'rsa:2048', '-days', '3650', '-nodes', '-x509',
+    '-subj', '/CN=selfsigned',
+    '-keyout', keyfile,
+    '-out', certfile,
+])
+c.NotebookApp.keyfile = keyfile
+c.NotebookApp.certfile = certfile
